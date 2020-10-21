d6da315c68766c67c60c71fc0bed1355df49fa07
sebastian-steindl <sebstein10@gmail.com>
2020-10-21 18:21:08 +0200

    Begin logic for puzzles and stones

---
+++ EscapeTheVR/Assets/ElementStone.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementStone
+{
+    public programmingElement elem; // e.g. for/if 
+    public int positionInProgram; // the position in which it should be placed
+
+    public string descriptionText; // text to describe the stone
+    public Color color; // color of the VR stone
+    public Color border; // border of the VR stone
+
+    public ElementStone()
+    {
+        descriptionText = "No description";
+        color = new Color(209, 186, 111); // should be changed to nice looking default later
+    }
+
+    public void greenBorder()
+    {
+        border = Color.green;
+    }
+    public void redBorder()
+    {
+        border = Color.red;
+    }
+}
+
+public class VariableStone : ElementStone
+{
+    public VariableStone()
+    {
+        elem = programmingElement.elemVar;
+    }
+}
+public enum programmingElement
+{
+    // feel free to add stuff
+    // use prefix "elem" bc if/while etc are forbidden
+    elemVar, // variable
+    elemFor,
+    elemIf,
+    elemWhile,
+    elemCompare, // ==
+    elemNegation, // !
+    elemLogicalAnd, // &&
+    elemLogicalOr, // ||
+    elemFuncPrint, // print()
+}
---
+++ EscapeTheVR/Assets/Puzzle.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle
+{
+    ElementStone[] elementStones; // the given order of programmingElements
+    programmingElement[] elementOrder; // the correct, expected order of programmingElements
+    List<int> wrongIndices; 
+
+    public Puzzle()
+    {
+
+    }
+
+    /*
+     * shouldHighlight: highlights Borders if true
+     * return: true if puzzle is correctly solved, false if not
+     */
+    public bool evaluatePuzzle(bool shouldHighlight)
+    {
+        this.wrongIndices = checkOrder();
+
+        if (shouldHighlight)
+        {
+            for (int i = 0; i < elementStones.Length; i++)
+            {
+                if (wrongIndices.Contains(i)) 
+                    elementStones[i].redBorder();
+                
+                else elementStones[i].greenBorder();
+            }
+
+        }
+
+        return wrongIndices.Count == 0;
+    }
+
+    /*
+     If wrongIndices.Count == 0 everything is fine, else those are the indices that are wrongly placed
+    could be highlighted etc in VR
+     */
+    List<int> checkOrder()
+    {
+        List<int> wrongIndices = new List<int>();
+
+        for (int i = 0; i < elementOrder.Length; i++)
+        {
+            // this should also work if their length is different, e.g. 4 stones are needed
+            // but only 3 are placed. 
+            if (elementStones[i].elem != elementOrder[i]) 
+            {
+                wrongIndices.Add(i);
+            }
+        }
+        
+
+        return wrongIndices;
+
+    }
+
+}
