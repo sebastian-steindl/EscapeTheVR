d86eddb5978525c31043c6ce0faced1afea2d86a
sebastian-steindl <sebstein10@gmail.com>
2020-10-22 19:31:20 +0200

    Move camera with mouse

---
+++ EscapeTheVR/Assets/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateCamByMouse();
+    }
+
+    void updateCamByMouse()
+    {
+        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+    }
+}
