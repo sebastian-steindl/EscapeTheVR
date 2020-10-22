e084ebf693bf950cd6d9c1a842093d5f0b0f7cf6
sebastian-steindl <sebstein10@gmail.com>
2020-10-22 20:05:34 +0200

    add steam vr, begin teleport

---
+++ EscapeTheVR/Assets/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    // https://github.com/jiankaiwang/FirstPersonController
+
+    public float speed = 10.0f;
+    private float translation;
+    private float straffe;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.GetAxis() is used to get the user's input
+        // You can furthor set it on Unity. (Edit, Project Settings, Input)
+        translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(straffe, 0, translation);
+    }
+}
+
