2a00b14df9d1ae855d3ecaa3100fa33176e768ae
sebastian-steindl <sebstein10@gmail.com>
2020-10-23 15:17:05 +0200

    add DragObject script

---
+++ EscapeTheVR/Assets/DragObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ https://www.youtube.com/watch?v=0yHBDZHLRbQ
+ */
+public class DragObject : MonoBehaviour
+{
+    //private Vector3 offset;
+    private float zCoord;
+
+    public GameObject gameBoard;
+    private void OnMouseDown()
+    {
+        //offset = gameObject.transform.position - GetMouseWorldPos();
+        zCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos();// + offset;
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = zCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
