b4ae55b3c38ad5c411d88028ffda300f34d6fb27
Sebastian Steindl <sebstein10@gmail.com>
2020-10-28 10:56:41 +0100

    Add constants

---
+++ EscapeTheVR/Assets/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    /*
+                Color constants 
+    */
+    public readonly Color colorPrint = new Color(255, 0, 0);
+    public readonly Color colorVar = new Color(0, 255, 0);
+
+
+    /*
+                string Constants 
+    */
+    public const string defaultDescription = "No desription";
+
+}
