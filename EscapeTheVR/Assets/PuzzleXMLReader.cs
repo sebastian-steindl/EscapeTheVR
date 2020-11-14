53177eef4323df1cf7050ffc4d219fabe644df77
LMoeges <lukas.moeges@hotmail.de>
2020-11-14 16:41:49 +0100

    Rewrite for checking puzzle with ids

--- EscapeTheVR/Assets/PuzzleXMLReader.cs
+++ EscapeTheVR/Assets/PuzzleXMLReader.cs
@@ -13,69 +13,16 @@ public class PuzzleXMLReader
             Debug.Log("Incorrect API-Version detected!");
         Puzzle p = new Puzzle(level.name, level.puzzleProgrammingElements.Count, snapEnabled);
         p.setSolution(getProgrammingElementsFromPuzzleProgrammingElements(level.puzzleProgrammingElements));
-        //TODO: Generate the CodeElements as DragObjects at the given coords.
-        level.puzzleProgrammingElements.ForEach(el => createElement(el));
         return (p, level);
     }
 
-    /*
-     If element could not be matched, the default datatype will be var.
-     public static programmingElement getProgrammingElementFromString(string element) {
-    programmingElement el = programmingElement.elemVar;
-        switch (element) {
-            case "elemCompare":
-                el =  programmingElement.elemCompare;
-                break;
-            case "elemFor":
-                el = programmingElement.elemFor;
-                break;
-            case "elemFuncPrint":
-                el = programmingElement.elemFuncPrint;
-                break;
-            case "elemIf":
-                el = programmingElement.elemIf;
-                break;
-            case "elemLogicalAnd":
-                el = programmingElement.elemLogicalAnd;
-                break;
-            case "elemLogicalOr":
-                el = programmingElement.elemLogicalOr;
-                break;
-            case "elemNegation":
-                el = programmingElement.elemNegation;
-                break;
-            case "elemVar":
-                el = programmingElement.elemVar;
-                break;
-            case "elemWhile":
-                el = programmingElement.elemWhile;
-                break;
-        }
-        return el;
-    }
-    */
-
-    internal static List<programmingElement> getProgrammingElementsFromPuzzleProgrammingElements(List<PuzzleProgrammingElement> puzzleElements) {
-        List<programmingElement> list = new List<programmingElement>();
+    internal static List<int> getProgrammingElementsFromPuzzleProgrammingElements(List<PuzzleProgrammingElement> puzzleElements) {
+        List<int> list = new List<int>();
         puzzleElements.ForEach(el => {
             if(el.isPartOfSolution)
-                list.Add(ElementStoneFactory.getProgrammingElementByString(el.type));
+                list.Add(el.id);
         });
         return list;
-    }
-
-
-    internal static DragObject createElement(PuzzleProgrammingElement puzzleElement) {
-        /*GameObject.CreatePrimitive(PrimitiveType.Quad);
-        var data = new GameObject("Hallo", typeof(DragObject));
-        data.AddComponent<DragObject>();
-        data.GetComponent<DragObject>();
-        DragObject drag = new DragObject(puzzleElement.type);
-        drag.transform.position = new Vector3(puzzleElement.positionX, puzzleElement.positionY, puzzleElement.positionZ);
-        return drag;*/
-
-
-        return null;
     }
 
 }
