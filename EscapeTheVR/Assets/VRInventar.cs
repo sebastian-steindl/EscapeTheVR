643b0ac315d38ca69eb03379a01a45c09cbb4021
sebastian-steindl <sebstein10@gmail.com>
2020-11-23 18:01:19 +0100

    singleton

---
+++ EscapeTheVR/Assets/VRInventar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRInventar
+{
+    private static VRInventar instance = new VRInventar();
+
+    private VRInventar()
+    {
+
+    }
+
+    public static VRInventar Instance
+    {
+        get { return instance; }
+    }
+
+
+
+}
