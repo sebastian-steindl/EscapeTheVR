bd5680135dda947f277eb0d0bbb9ee82c60e85ab
LMoeges <lukas.moeges@hotmail.de>
2020-11-24 20:04:50 +0100

    Bugfix: changed selection will now result in correctly displayed data.

--- EscapeTheVR/Assets/LevelDetailPage.cs
+++ EscapeTheVR/Assets/LevelDetailPage.cs
@@ -3,21 +3,18 @@ using UnityEngine;
 
 public class LevelDetailPage:Page
 {
-    public void Start()
+    private void Update()
     {
-
         var level = LevelManager.Instance().getLevelById(PlayerPrefs.GetInt("level", 1));
         var textFields = GetComponentsInChildren<TMP_Text>();
 
         //Fullfill text fields.
         foreach (TMP_Text textfield in textFields)
         {
-            Debug.Log(textfield.name);
             if (textfield.name == "LevelName")
                 textfield.text = level.name;
             if (textfield.name == "LevelDescription")
                 textfield.text = level.descr;
         }
-
     }
 }
