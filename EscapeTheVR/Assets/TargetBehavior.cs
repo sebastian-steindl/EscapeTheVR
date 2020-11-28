8c8ede49969c3e59e0eb5ab9b64432d9ae2cead3
Moritz <moritz.pruefling@gmail.com>
2020-11-28 15:57:28 +0100

    Added aiming target feature

---
+++ EscapeTheVR/Assets/TargetBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehavior : MonoBehaviour
+{
+    public GameObject ControlledObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.ControlledObject.GetComponent<Rigidbody>().useGravity = false;
+        this.ControlledObject.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.ControlledObject.GetComponent<Rigidbody>().isKinematic = false;
+        this.ControlledObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
