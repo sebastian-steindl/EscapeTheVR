3dbfae109ffcebd7b31d9aa2d7e2d59bf8d598fb
Moritz <moritz.pruefling@gmail.com>
2020-12-02 16:55:11 +0100

    Added hint system for element stones

---
+++ EscapeTheVR/Assets/HintObjectBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintObjectBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var elementStone = other.gameObject.GetComponent<DragObject>()?.element;
+        if (elementStone == null) return;
+
+        elementStone.PlayHintSound();
+    }
+}
