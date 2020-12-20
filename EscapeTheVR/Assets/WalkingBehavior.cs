ceea7b6bc61a8359ffc9aa7b518eca2d18b0c54e
Moritz <moritz.pruefling@gmail.com>
2020-12-20 23:01:22 +0100

    Added animation triggering for teacher object

---
+++ EscapeTheVR/Assets/WalkingBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        var anim = gameObject.GetComponent<Animation>();
+        anim.Play("Armature|walking");
+    }
+}
