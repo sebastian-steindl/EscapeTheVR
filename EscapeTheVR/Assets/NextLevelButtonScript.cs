04343cb4cb2ef33f869aaec2a0fe57644c11ccc6
LMoeges <lukas.moeges@hotmail.de>
2020-12-21 20:42:05 +0100

    Added NextLevelButtonScript that is necessary for commit 0b9d6be. :$

---
+++ EscapeTheVR/Assets/NextLevelButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("We are going to change the scene.");
+        if(FindObjectOfType<GameBoardScript>().puzzle.isSolved)
+            SceneManager.LoadScene("ChangeScene");
+    }
+
+    //I'm not sure if this will be necessary for the button to work in VR...
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnterCalled!");
+        OnMouseDown();
+    }
+}
